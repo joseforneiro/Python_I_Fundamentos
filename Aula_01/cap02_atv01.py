e990d5d1b190decbdadbebeac1cdd32d20549089
José Francisco Forneiro Junior <51200164+joseforneiro@users.noreply.github.com>
2026-05-05 22:24:42 -0300

    Create cap02_atv01.py

---
+++ Aula_01/cap02_atv01.py
@@ -0,0 +1,6 @@
+numero1 = input("Digite o primeiro número da soma: ")
+numero2 = input("Digite o segundo número da soma: ")
+
+soma = float(numero1) + float(numero2)
+
+print(f"O resultado da soma é {soma:.2f}")
