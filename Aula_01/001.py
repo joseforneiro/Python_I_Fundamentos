a79f2c2cdc2576a14a266057ed28c3a680280707
José Francisco Forneiro Junior <51200164+joseforneiro@users.noreply.github.com>
2026-05-05 22:13:10 -0300

    primeiro commit

--- Aula_01/001.py
+++ Aula_01/001.py
@@ -1,1 +1,8 @@
-print("Olá Mundo!")
+# print("Olá Mundo!")
+
+nome = "João" # string
+idade = 27 # int
+
+# print("Olá, "+ nome +" você tem "+ idade +" anos") # Precisa converter todas as variáveis para string
+print("Olá, ", nome , " você tem ", idade ," anos") # Não precisa converter variáveis para string
+print(f"Olá, {nome} você tem {idade} anos") # f string
---
+++ Aula_01/cap1_atv01.py
@@ -0,0 +1,7 @@
+nome = input("Nome: ")
+idade = input("Idade: ")
+cidade = input("Cidade: ")
+
+print(f"Olá {nome}!")
+print(f"Você tem {idade} anos e mora em {cidade}.")
+print("Bem-vindo ao mundo da programação Python!")
