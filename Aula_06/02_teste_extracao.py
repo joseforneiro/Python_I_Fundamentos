679415a260d0bf093b7b4cf217bff97f2b12d6cb
José Francisco Forneiro Junior <51200164+joseforneiro@users.noreply.github.com>
2026-05-18 22:15:55 -0300

    Update 02_teste_extracao.py

--- Aula_06/02_teste_extracao.py
+++ Aula_06/02_teste_extracao.py
@@ -7,4 +7,8 @@ requisicao = requests.get(url)
 
 site = BeautifulSoup(requisicao.text, "html.parser") # Transforma o texto em html
 
+# titulos = site.find_all("h3")
+# for t in titulos:
+#     print(t.text)
+
 print(site.prettify()) # organiza o texto html
